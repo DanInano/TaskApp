0bee1fef2c4b69a5783e56a93bc35629ae9b3796
dan.inano <dan.inano@icloud.com>
2020-08-29 09:26:03 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by Dan Inano on 2020/08/29.
+//  Copyright © 2020 Dan Inano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
